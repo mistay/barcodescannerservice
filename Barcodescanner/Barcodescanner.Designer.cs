0bbdb25c82d6ac74f999348b1d5bc7504b4c61f2
armin langhofer <armin@langhofer.at>
2022-03-04 09:40:20 +0100

    init commit

---
+++ Barcodescanner/Barcodescanner.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Barcodescanner
+{
+    partial class Barcodescanner
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived_1);
+            // 
+            // Barcodescanner
+            // 
+            this.ServiceName = "Barcodescanner";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
---
+++ Barcodescanner/Barcodescanner.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Net.Http;
+
+namespace Barcodescanner
+{
+    public partial class Barcodescanner : ServiceBase
+    {
+        public Barcodescanner()
+        {
+            InitializeComponent();
+
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        private static readonly HttpClient httpClient = new HttpClient();
+
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart. version 7");
+
+            serialPort1.Open();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop.");
+        }
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }
+
+        private void serialPort1_DataReceived_1(object sender, SerialDataReceivedEventArgs e)
+        {
+            System.IO.Ports.SerialDataReceivedEventArgs args = (System.IO.Ports.SerialDataReceivedEventArgs)e;
+
+            string foo = serialPort1.ReadExisting();
+            eventLog1.WriteEntry("read byte: " + foo.Replace('%', ' '));
+
+
+            Task task = sendHttpAsync(foo);
+
+        }
+        async Task sendHttpAsync(string barcode)
+        {
+            eventLog1.WriteEntry("sendHttpAsync()");
+
+            var values = new Dictionary<string, string>
+            {
+                { "barcode", barcode },
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+
+            eventLog1.WriteEntry("trying to sendHttpAsync() ...");
+            var response = await httpClient.PostAsync("https://langhofer.at/foo.php", content);
+            eventLog1.WriteEntry("sendHttpAsync() sent.");
+
+        }
+    }
+}
